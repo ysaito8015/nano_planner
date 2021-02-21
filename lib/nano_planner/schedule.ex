a827ead3537cf8873f29144b8c94ce6aa2dbb006
Yusuke_Saito <ysaito8015@gmail.com>
2021-02-21 15:15:48 +0100

    done ch04

--- lib/nano_planner/schedule.ex
+++ lib/nano_planner/schedule.ex
@@ -14,13 +14,26 @@ defmodule NanoPlanner.Schedule do
     t1 = t0 |> Timex.shift(hours: 24)
 
     PlanItem
-    |> where([i], i.starts_at >= ^t0 and i.starts_at < ^t1)
+    |> where(
+      [i],
+      (i.starts_at >= ^t0 and i.starts_at < ^t1) or
+        (i.ends_at > ^t0 and i.ends_at <= ^t1)
+    )
+    |> fetch_plan_items()
+  end
+
+  def list_continued_plan_items do
+    t0 = current_time() |> Timex.beginning_of_day()
+    t1 = t0 |> Timex.shift(hours: 24)
+
+    PlanItem
+    |> where([i], i.starts_at < ^t0 and i.ends_at > ^t1)
     |> fetch_plan_items()
   end
 
   defp fetch_plan_items(query) do
     query
-    |> order+by(asc: :starts+at, asc: :ends+at, asc: :id)
+    |> order_by(asc: :starts_at, asc: :ends_at, asc: :id)
     |> Repo.all()
     |> convert_datetime()
   end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -41,5 +41,5 @@ insert!(%PlanItem{
   name: "姉の出張",
   description: "札幌",
   starts_at: Timex.shift(time0, years: -1, days: 10),
-  ends_at: Timex.shift(time0, hours: 17)
+  ends_at: Timex.shift(time0, days: 1, hours: 17)
 })
